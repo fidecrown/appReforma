5587c082a2b46aef33ed016fc7b2d095f059de59
fidencio corona <corf1985@gmail.com>
2024-06-28 16:00:43 -0600

    CAMBIAMOS LA INYECCION: @Autowired, E INYECTAMOS EL SERVICIO MEDIANTE UN CONSTRUCTOR

--- src/main/java/com/web/api/appreforma/controladores/RegistroSIngresoController.java
+++ src/main/java/com/web/api/appreforma/controladores/RegistroSIngresoController.java
@@ -2,7 +2,6 @@ package com.web.api.appreforma.controladores;
 
 import com.web.api.appreforma.entidades.RegistroSIngreso;
 import com.web.api.appreforma.servicios.RegistroSIngresoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +11,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "reforma/api/v1/registroSolicitud")
 public class RegistroSIngresoController {
 
-    @Autowired
-    private RegistroSIngresoService registroSIngresoService;
+    private final RegistroSIngresoService registroSIngresoService;
+
+    public RegistroSIngresoController(RegistroSIngresoService registroSIngresoService) {
+        this.registroSIngresoService = registroSIngresoService;
+    }
 
     @PostMapping("")
     public ResponseEntity<?> saveRegistroIngreso(@RequestBody RegistroSIngreso entidad){
@@ -21,7 +23,6 @@ public class RegistroSIngresoController {
             registroSIngresoService.save(entidad);
             return ResponseEntity.status(HttpStatus.OK).body("{\"msj\" : \"Solicitud de Ingreso Guardada Correctamente.\"}");
         }catch (Exception e){
-            System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\" : \"Error por favor intente mas tarde.\"}");
         }
     }
